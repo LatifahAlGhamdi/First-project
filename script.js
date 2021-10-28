3c8c08d79831805d5fb10928745187bafc3935ad
LatifahAlGhamdi <Latifah.Dafer.abt.js@tuwaiq.edu.sa>
2021-10-28 09:10:40 +0300

    First project done

---
+++ script.js
@@ -0,0 +1,12 @@
+//connectWithUs
+const button = document.querySelectorAll(".connectWithUs .btn2")
+// console.log(button)
+const connectWithUs = document.querySelector(".connectWithUs")
+// console.log(connectWithUs)
+
+
+button[1].addEventListener("click", function () {
+    connectWithUs.innerHTML = "<h1> Thank you for content </h1>"
+
+})
+
